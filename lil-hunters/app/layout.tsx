6c1da9c98e6fc9dd42c604cf02e08334bf46f129
aidan.newell <aidan.newell@river.com>
2023-07-02 11:21:19 -0500

    navbar

--- lil-hunters/app/layout.tsx
+++ lil-hunters/app/layout.tsx
@@ -1,7 +1,5 @@
+import NavBar from "@/components/NavBar";
 import "./globals.css";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Lil Bounty Hunters",
@@ -15,7 +13,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className="relative">
+        <NavBar />
+        {children}
+      </body>
     </html>
   );
 }
