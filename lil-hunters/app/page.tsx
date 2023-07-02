ad603f6426a12793f585d353609812d718071f62
aidan.newell <aidan.newell@river.com>
2023-07-02 10:27:47 -0500

    next skeleton project init

---
+++ lil-hunters/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      {/* <NavBar /> */}
+
+      <h1 className="text-xl font-bold">Lil Bounty Hunters</h1>
+      <p>A learner project by extrapockets & bitdern</p>
+    </main>
+  );
+}
