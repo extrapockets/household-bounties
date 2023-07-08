899a8b374bc38a84bb892ffdab7ac49903d9500f
aidan.newell <aidan.newell@river.com>
2023-07-08 11:55:36 -0500

    added a nice nav component with buttons

--- lil-hunters/app/page.js
+++ lil-hunters/app/page.js
@@ -3,9 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="overflow-hidden">
-      <div className="mt-12 py-6">
+      <div className="mt-25 py-6">
         <h1 className="text-xl font-bold">Lil Bounty Hunters</h1>
-        <p>A learner project by extrapockets & bitdern</p>
       </div>
     </main>
   );
--- lil-hunters/components/NavBar.jsx
+++ lil-hunters/components/NavBar.jsx
@@ -4,17 +4,42 @@ import Link from "next/link";
 const NavBar = () => {
   return (
     <header className="w-full absolute z-10">
-      <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4">
+      <div className="navbar bg-base-100">
         <Link href="/" className="flex justify-center items-center">
           <Image
             src="/bounty-hunter.png"
             alt="app logo"
-            width={50}
-            height={50}
-            className="object-contain bg-orange-400 rounded-sm"
+            width={55}
+            height={55}
+            className="object-contain bg-orange-400 rounded-full"
           />
         </Link>
-      </nav>
+        <div className="flex-1">
+          <a className="btn btn-ghost normal-case text-xl">
+            Lil Bounty Hunters
+          </a>
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <a>Sign Up!</a>
+            </li>
+            <li>
+              <details>
+                <summary>Login</summary>
+                <ul className="p-2 bg-base-100">
+                  <li>
+                    <a>Maker</a>
+                  </li>
+                  <li>
+                    <a>Hunter</a>
+                  </li>
+                </ul>
+              </details>
+            </li>
+          </ul>
+        </div>
+      </div>
     </header>
   );
 };
