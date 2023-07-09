fee019eec97185f0f55ba84deca523cd5a3fe844
aidan.newell <aidan.newell@river.com>
2023-07-09 16:25:14 -0500

    got the task boxes to sit inside the fields properly

--- lil-hunters/components/TaskCard.jsx
+++ lil-hunters/components/TaskCard.jsx
@@ -5,14 +5,14 @@ import React from "react";
 const TaskCard = () => {
   return (
     <div className="px-5 space-x-4 card bg-slate-800 rounded-box place-items-center">
-      <div className="flex m-4">
-        <div className="flex-grow m-4">
+      <div className="flex flex-grow justify-evenly px-4">
+        <div>
           <p className="text-md font-semibold">Task Title</p>
-          <div className="flex justify-between">
+          <div className="flex">
             <button className="btn btn-xs btn-outline">Details</button>
           </div>
         </div>
-        <div className="flex-grow m-4">
+        <div>
           <p className="text-sm">[amount]sats</p>
         </div>
         <input
@@ -20,14 +20,14 @@ const TaskCard = () => {
           placeholder="Enter post URL here..."
           className="input input-bordered input-sm w-full max-w-xs m-4"
         />
-        <div className="flex-grow m-4">
+        <div className="m-2">
           <input
             type="checkbox"
             checked="checked"
             className="checkbox form-control"
           />
         </div>
-        <div className="flex-grow m-4">
+        <div className="m-2">
           <input
             type="checkbox"
             checked="checked"
