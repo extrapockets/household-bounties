640ca946c9d8ec7378017112979caffb10432427
extrapockets <extrapockets@proton.me>
2023-07-17 23:03:22 -0500

    Change color of text on navigation bars
    Use on-navbar attribute to change black text to orange for readability

--- lil-hunters/components/TaskCard.jsx
+++ lil-hunters/components/TaskCard.jsx
@@ -7,13 +7,13 @@ const TaskCard = () => {
     <div className="px-5 space-x-4 card bg-slate-800 rounded-box">
       <div className="flex flex-grow justify-evenly px-4">
         <div>
-          <p className="text-md font-semibold">Task Title</p>
+          <p className="text-md font-semibold on-navbar">Task Title</p>
           <div className="flex">
-            <button className="btn btn-xs btn-outline">Details</button>
+            <button className="btn btn-xs btn-outline on-navbar">Details</button>
           </div>
         </div>
         <div>
-          <p className="text-sm ">[amount]sats</p>
+          <p className="text-sm on-navbar">[amount]sats</p>
         </div>
         <input
           type="text"
